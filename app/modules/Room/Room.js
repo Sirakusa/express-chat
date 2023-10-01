079d22365ba3933743544db388b30c17fb179aed
Sirakusa <herrera.clip@gmail.com>
2023-09-30 22:26:31 -0600

    add room repository

---
+++ app/modules/Room/Room.js
@@ -0,0 +1,41 @@
+class RoomRepository {
+  constructor() {
+    this.room = [];
+    this.id = 1;
+  }
+
+  create(user1, user2) {
+    this.room.push({
+      id: this.id,
+      user1,
+      user2,
+      createdAt: new Date(),
+    });
+
+    this.id += 1;
+  }
+
+  read(id) {
+    const idRoom = this.room.find((post) => post.id === id);
+
+    if (!idRoom) {
+      return `room ${id} is not defined`;
+    }
+
+    return this.room[id];
+  }
+
+  delete(id) {
+    const idRoom = this.room.find((post) => post.id === id);
+
+    if (!idRoom) {
+      return `room ${id} is not defined`;
+    }
+
+    this.room.splice(id, 1);
+
+    return 'successful';
+  }
+}
+
+module.exports = RoomRepository;
