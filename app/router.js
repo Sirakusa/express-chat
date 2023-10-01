3ff112ab7dd98493a5bf0833d32579952d8673a6
Sirakusa <herrera.clip@gmail.com>
2023-10-01 01:26:18 -0600

    router of messages

--- app/router.js
+++ app/router.js
@@ -1,9 +1,9 @@
-const routerRoom = require('./modules/Room/router.js');
-const routerMessage = require('./modules/Message/router.js');
+const routerRoom = require('./modules/Room/router');
+const routerMessage = require('./modules/Message/router');
 
 module.exports = (app) => {
   app.use('/room', routerRoom);
-  app.use('/room', routerRoom);
+  app.use('/message', routerMessage);
 
   app.use((req, res) => {
     res.status(404).send('This route it is not being supported yet!');
